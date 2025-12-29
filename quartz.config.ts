cd9f104f051e8d871f529575025dc5c8b0934651
暄惠 黃 <huangxh8531@gmail.com>
2025-12-29 15:01:48 +0800

    Secure Sync: 2025-12-29 15:01

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 我的數位花園", // 這是網頁左上角的標題
+    pageTitle: "H.XuanHui's Notes", // 這是網頁左上角的標題
     pageTitleSuffix: " | 知識庫",
     enableSPA: true,
     enablePopovers: true,
